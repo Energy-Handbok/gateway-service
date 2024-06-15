41db1bb3f5ec57b7fb7d043dee1eaf777fba81fd
Pham Huynh Phuong Kha <vuamessi9@gmail.com>
2024-06-15 15:19:04 +0700

    build: add endpoint caculate bmi & tdee from food service

--- src/main/java/com/khaphp/gateway_service/endpoint/FoodServiceEndpoint.java
+++ src/main/java/com/khaphp/gateway_service/endpoint/FoodServiceEndpoint.java
@@ -10,12 +10,21 @@ import java.util.Map;
 
 public class FoodServiceEndpoint {
     public static final String PATH_SERVICE_FOOD = "/api/v1/food";
+    public static final String PATH_SERVICE_FOOD_CALCULATE_BMI_TEDEE = "/energy";
     public static final String PATH_SERVICE_FOOD_ENCYCLOPEDIA = "/api/v1/food-encyclopedia";
     private Map<String, List<String>> urlAutho; //Map<method-url, roles>
 
     public Map<String, List<String>> urlPermission() {  //lazy initialization
         if(urlAutho == null){
             urlAutho = new HashMap<>();
+
+            //--PATH_SERVICE_FOOD_CALCULATE_BMI_TEDEE
+            urlAutho.put(HttpMethod.GET + "-" + PATH_SERVICE_FOOD_CALCULATE_BMI_TEDEE + "/bmi",
+                    List.of(Publich.PUBLICH.name()));
+
+            urlAutho.put(HttpMethod.GET + "-" + PATH_SERVICE_FOOD_CALCULATE_BMI_TEDEE + "/tdee",
+                    List.of(Publich.PUBLICH.name()));
+
             for(String path : List.of(PATH_SERVICE_FOOD, PATH_SERVICE_FOOD_ENCYCLOPEDIA)){
                 urlAutho.put(HttpMethod.GET + "-" + path,
                         List.of(Publich.PUBLICH.name()));
